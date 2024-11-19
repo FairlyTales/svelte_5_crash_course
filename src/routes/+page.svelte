97fbe85cbc6a0911124e1ba26ff939f1c32a0beb
khnychkin_kirill <kirill.khnychkin@gmail.com>
2024-11-19 19:35:16 +0400

    Add header component with prop. Add input with value bind. Add button with handler

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Header from './Header.svelte';
+
+  let name = $state('Name');
+
+  const onclick = () => {
+    console.log('clicked');
+  };
+</script>
+
+<Header {name} />
+
+<input type="text" bind:value={name}>
+<button {onclick}>button</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  interface HeaderProps {
+    name: string;
+  }
+
+  let { name }: HeaderProps = $props();
+</script>
+
+<h1>header</h1>
+<div>{name}</div>
